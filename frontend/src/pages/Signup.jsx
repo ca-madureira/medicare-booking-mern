4adc39ab31d810dd5874249fec4a68630a35f93e
Camila Madureira <121184472+ca-madureira@users.noreply.github.com>
2023-10-08 13:32:48 -0300

    feat: create upload images

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import signupImg from '../assets/images/signup.gif';
-import avatar from '../assets/images/doctor-img01.png';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import uploadImageToCloudinary from "../utils/uploadCloudinary";
+import { BASE_URL } from "../config";
+import { toast } from "react-toastify";
+import signupImg from "../assets/images/signup.gif";
+import avatar from "../assets/images/doctor-img01.png";
+import HashLoader from "react-spinners/HashLoader";
 
 const Signup = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [previewURL, setPreviewURL] = useState('');
+  const [previewURL, setPreviewURL] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
     photo: selectedFile,
-    gender: '',
-    role: 'patient',
+    gender: "",
+    role: "patient",
   });
+
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -23,140 +29,176 @@ const Signup = () => {
 
   const handleFileInputChange = async (event) => {
     const file = event.target.files[0];
+
+    const data = await uploadImageToCloudinary(file);
+
+    setPreviewURL(data.url);
+    setSelectedFile(data.url);
+    setFormData({ ...formData, photo: data.url });
   };
 
   const submitHandler = async (event) => {
     event.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${BASE_URL}/api/v1/auth/register`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const { message } = await res.json();
+
+      if (!res.ok) {
+        throw new Error(message);
+      }
+
+      setLoading(false);
+      toast.success(message);
+      navigate("/login");
+    } catch (err) {
+      toast.error(err.message);
+      setLoading(false);
+    }
   };
   return (
-    <section className="px-5 xl:px-0">
-      <div className="max-w-[1170px] mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-          <div className="hidden lg:block bg-primaryColor rounded-l-lg">
-            <figure className="rounded-l-lg">
-              <img src={signupImg} alt="" className="w-full rounded-l-lg" />
+    <section className='px-5 xl:px-0'>
+      <div className='max-w-[1170px] mx-auto'>
+        <div className='grid grid-cols-1 lg:grid-cols-2'>
+          <div className='hidden lg:block bg-primaryColor rounded-l-lg'>
+            <figure className='rounded-l-lg'>
+              <img src={signupImg} alt='' className='w-full rounded-l-lg' />
             </figure>
           </div>
 
-          <div className="rounded-l-lg lg:pl-16 py-10">
-            <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
-              Criar um <span className="text-primaryColor">conta</span>
+          <div className='rounded-l-lg lg:pl-16 py-10'>
+            <h3 className='text-headingColor text-[22px] leading-9 font-bold mb-10'>
+              Criar um <span className='text-primaryColor'>conta</span>
             </h3>
 
             <form>
-              <div className="mb-5">
+              <div className='mb-5'>
                 <input
-                  type="text"
-                  placeholder="Nome completo"
-                  name="name"
+                  type='text'
+                  placeholder='Nome completo'
+                  name='name'
                   value={formData.name}
                   onChange={handleInputChange}
-                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer'
                   required
                 />
               </div>
-              <div className="mb-5">
+              <div className='mb-5'>
                 <input
-                  type="email"
-                  placeholder="Entre com seu email"
-                  name="email"
+                  type='email'
+                  placeholder='Entre com seu email'
+                  name='email'
                   value={formData.email}
                   onChange={handleInputChange}
-                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer'
                   required
                 />
               </div>
-              <div className="mb-5">
+              <div className='mb-5'>
                 <input
-                  type="password"
-                  placeholder="Senha"
-                  name="password"
+                  type='password'
+                  placeholder='Senha'
+                  name='password'
                   value={formData.password}
                   onChange={handleInputChange}
-                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer'
                   required
                 />
               </div>
-              <div className="mb-5">
+              <div className='mb-5'>
                 <input
-                  type="password"
-                  placeholder="Senha"
-                  name="password"
-                  value=""
-                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  type='password'
+                  placeholder='Senha'
+                  name='password'
+                  value=''
+                  className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus: outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer'
                   required
                 />
               </div>
-              <div className="mb-5 flex items-center justify-between">
+              <div className='mb-5 flex items-center justify-between'>
                 <label
-                  htmlFor=""
-                  className="text-headingColor font-bold text-[16px] leading-7"
+                  htmlFor=''
+                  className='text-headingColor font-bold text-[16px] leading-7'
                 >
                   Você é um:
                   <select
-                    name="role"
+                    name='role'
                     value={formData.role}
                     onChange={handleInputChange}
-                    className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                    className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none'
                   >
-                    <option value="patient">Paciente</option>
-                    <option value="doctor">Doctor</option>
+                    <option value='patient'>Paciente</option>
+                    <option value='doctor'>Doctor</option>
                   </select>
                 </label>
                 <label
-                  htmlFor=""
-                  className="text-headingColor font-bold text-[16px] leading-7"
+                  htmlFor=''
+                  className='text-headingColor font-bold text-[16px] leading-7'
                 >
                   Genero:
                   <select
-                    name="gender"
+                    name='gender'
                     value={formData.gender}
                     onChange={handleInputChange}
-                    className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                    className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none'
                   >
-                    <option value="">Select</option>
-                    <option value="male">Masculino</option>
-                    <option value="female">Feminino</option>
-                    <option value="other">Outro</option>
+                    <option value=''>Select</option>
+                    <option value='male'>Masculino</option>
+                    <option value='female'>Feminino</option>
+                    <option value='other'>Outro</option>
                   </select>
                 </label>
               </div>
 
-              <div className="mb-5 flex items-center gap-3">
-                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-                  <img src={avatar} alt="" className="w-full rounded-full" />
-                </figure>
+              <div className='mb-5 flex items-center gap-3'>
+                {selectedFile && (
+                  <figure className='w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center'>
+                    <img
+                      src={previewURL}
+                      alt=''
+                      className='w-full rounded-full'
+                    />
+                  </figure>
+                )}
 
-                <div className="relative w-[130px] h-[50px]">
+                <div className='relative w-[130px] h-[50px]'>
                   <input
-                    type="file"
-                    name="photo"
-                    id="customFile"
-                    accept=".jpg, .png"
-                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                    type='file'
+                    name='photo'
+                    id='customFile'
+                    accept='.jpg, .png'
+                    className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer'
                   />
                   <label
-                    htmlFor="customFile"
-                    className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+                    htmlFor='customFile'
+                    className='absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer'
                   >
                     Carregar imagem
                   </label>
                 </div>
               </div>
-              <div className="mt-7">
+              <div className='mt-7'>
                 <button
-                  type="submit"
-                  className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-2"
+                  disabled={loading && true}
+                  type='submit'
+                  className='w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-2'
                 >
-                  Cadastrar
+                  {loading ? <HashLoader size={35} color='#fff' /> : "Sign Up"}
                 </button>
               </div>
 
-              <p className="mt-5 text-textColor text-center">
+              <p className='mt-5 text-textColor text-center'>
                 Já tem conta?
                 <Link
-                  to="/register"
-                  className="text-primaryColor font-medium ml-1"
+                  to='/register'
+                  className='text-primaryColor font-medium ml-1'
                 >
                   Cadastrar
                 </Link>
